82613f697b17965140b42b04f0f3949b7b8b2184
斌斌斌 <1587443838@qq.com>
2020-02-13 08:30:30 +0800

    发布订阅 dialog

--- zhufengvue/34emit-on.js
+++ zhufengvue/34emit-on.js
@@ -1,14 +1,17 @@
+//发布 emit 订阅 on {}
 function Girl() {
     this._events = {}
 }
 Girl.prototype.on = function (eventName,callback) {
-    if(this._events[eventName]){
-        this._events[eventName].push(callback)
+    if(this._events[eventName]){//不是第一次
+        this._events[eventName].push(callback);//{失恋:[cry,eat,shopping]}
     }else{
-        this._events[eventName] = [callback]
+        this._events[eventName] = [callback]//{失恋:[cry]}
     }
 }
-Girl.prototype.emit = function (evevtName,...args) {
+Girl.prototype.emit = function (evevtName,...args) {//[我，你，他]
+    // [].slice.call(arguments,1);
+    // Array.from(arguments).slice(1)
     if(this._events[evevtName]){
         this._events[evevtName].forEach(cb =>cb(...args))
     }
@@ -23,7 +26,7 @@ let shopping = (who)=>{
 let eat = (who)=>{
     console.log(who+'吃')
 };
-girl.on('失恋',cry);
+girl.on('失恋',cry);//{失恋:[cry]}
 girl.on('失恋',shopping);
 girl.on('失恋',eat);
 girl.emit('失恋','我','你')
